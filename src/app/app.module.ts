6f4a366ec7f7cddafdc68ba3e11ba12901a8875b
mmullerx4 <mmullerx4@gmail.com>
2024-09-28 16:49:02 -0400

    recipe setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './header/shopping/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './header/shopping/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './header/recipeBook/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './header/recipeBook/recipe-item/recipe-item.component';
+import { recipeDetailComponent } from './header/recipeBook/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    recipeDetailComponent
   ],
   imports: [
     BrowserModule

79134e7f69299182b9f1409109a3269a5d9fe982
mmullerx4 <mmullerx4@gmail.com>
2024-10-01 10:48:33 -0400

    moved recipe item inside recipe list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './header/shopping/shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './header/shopping/shopping-list-edit/shopping-list-edit.component';
 import { RecipeListComponent } from './header/recipeBook/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './header/recipeBook/recipe-item/recipe-item.component';
+import { RecipeItemComponent } from './header/recipeBook/recipe-list/recipe-item/recipe-item.component';
 import { recipeDetailComponent } from './header/recipeBook/recipe-detail/recipe-detail.component';
 import { RecipeBookComponent } from './header/recipeBook/recipeBook.component';
 
